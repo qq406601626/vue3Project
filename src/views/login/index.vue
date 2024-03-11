b1fa6e39afa827b81e74e61ae02ba20997728611
guoshuai <406601626@qq.com>
2024-03-11 22:38:45 +0800

    测试

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,38 +1,29 @@
 <script setup>
+import {ref, reactive, watchEffect, watch, readonly, isRef,toRef,isReactive} from 'vue'
+import {useForm} from './useForm'
+import {Text} from 'vue'
 
-import {ref} from 'vue'
-const formData = ref({
-  account:'',
-  password:''
-})
-const a = ref('zhangsan')
-const b = ref('lisi')
-const $elForm = ref()
-const submit = async (aaaa)=>{
-  try {
-   // await $elForm.value.validate()
-   //  await form.validate()
-   //  console.log('成功')
-    console.log(aaaa)
-    console.log(b)
-  }catch (e){
-    // console.log('失败')
-  }
+const {formData, _form, submit} = useForm()
+const btn = ref()
+const log = () => {
+  console.log(btn.value.shouldAddSpace)
 }
 </script>
 
+
 <template>
-<el-form ref="$elForm" :model="formData">
-  <el-form-item prop="account" :rules="[{required:true,message:'请输入账号',trigger:'blur'}]">
-    <el-input v-model="formData.account"></el-input>
-  </el-form-item>
-  <el-form-item prop="password" :rules="[{required:true,message:'请输入账号',trigger:'blur'}]">
-    <el-input v-model="formData.password"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button @click="submit(a)">submit</el-button>
-  </el-form-item>
-</el-form>
+  <el-form ref="_form" :model="formData">
+    <el-form-item prop="account" :rules="[{required:true,message:'请输入账号',trigger:'blur'}]">
+      <el-input v-model="formData.account"></el-input>
+    </el-form-item>
+    <el-form-item prop="password" :rules="[{required:true,message:'请输入账号',trigger:'blur'}]">
+      <el-input v-model="formData.password"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button @click="submit()">submit</el-button>
+      <el-button ref="btn" @click="log" auto-insert-space>打印</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <style scoped>
---
+++ src/views/login/test.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {defineProps,toRef} from 'vue'
+const props = defineProps({
+  userName:String
+})
+// const userName2 = toRef(props,'userName')
+// console.log('userName2',userName2)
+// userName2.value = 'lisi'
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
