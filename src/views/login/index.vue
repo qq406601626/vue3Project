03b8281f1ac79a59fd9038b51d611e4d4b7ad53a
guoshuai <406601626@qq.com>
2024-03-20 22:46:52 +0800

    测试

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,32 +1,9 @@
 <script setup>
-import {useCssModule} from 'vue'
-const module = useCssModule('test')
-console.log(module)
+console.log(import.meta.env.VITE_SOME_KEY)
 </script>
-<template>
-  <div :class="test.a">eeeeeeeeeeee</div>
-  <div :class="test.container">
-    <span :class="test.a">aaaaaa</span>
-  </div>
-  <div :id="test.container">qqqqqqq</div>
-</template>
+<template></template>
 
 <style scoped lang="scss">
 
 </style>
-<style module="test" >
-.a{
-  color: red;
-}
-.container{
-  background: blue;
-  padding: 20px;
-  .a {
-    background: gray;
-    color: aqua;
-  }
-}
-#container{
-  background: pink;
-}
-</style>
+
