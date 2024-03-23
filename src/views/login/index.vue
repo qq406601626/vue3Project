2a59b02decc9f44d7965d4fb0f87e3a929f3ae84
guoshuai <406601626@qq.com>
2024-03-23 13:54:32 +0800

    测试

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,9 @@
 <script setup>
 console.log(import.meta.env.VITE_SOME_KEY)
 </script>
-<template></template>
+<template>
+  <h1>Vite is running in %MODE%</h1>
+</template>
 
 <style scoped lang="scss">
 
