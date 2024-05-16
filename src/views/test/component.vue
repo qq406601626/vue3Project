0108844a7fca7a2d3934fed57da3d2082e82d59e
guoshuai <406601626@qq.com>
2024-05-16 14:42:51 +0800

    修改

---
+++ src/views/test/component.vue
@@ -0,0 +1,41 @@
+<script>
+import {ref} from  'vue'
+export default {
+  setup(){
+    const print = function(){
+      console.log('print1')
+    }
+
+
+    return {
+      print,
+    }
+  },
+  data(){
+    return {
+      age:18,
+      addr:'shanghai'
+    }
+  },
+  methods:{
+    print2(){
+      console.log('print2')
+    },
+    print3(){
+      console.log('print3')
+    }
+  }
+}
+</script>
+<template>
+<div class="component-container">
+  <div @click="print">1111</div>
+  <div @click="print2">2222</div>
+  <div >age:{{age}}</div>
+  <div>addr:{{addr}}</div>
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
